5d732fb4f8e1425a797141e4c0c75cabe083b168
MayurMansukh <Mansukhmayur99@gmail.com>
2021-03-04 11:26:08 +0530

    Create mood analyzer check happy or sad

---
+++ src/main/java/MoodAnalyzerJunit/MoodAnalyser.java
@@ -0,0 +1,22 @@
+package MoodAnalyzerJunit;
+
+public class MoodAnalyser {
+    private String message;
+
+    public MoodAnalyser(String message)
+    {
+        this.message=message;
+    }
+
+    public String analyseMood()
+    {
+        if(message.contains("sad"))
+        {
+            return "Sad";
+        }
+        else
+        {
+            return "Happy";
+        }
+    }
+}
