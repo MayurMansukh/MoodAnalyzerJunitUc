8a3007f01f322400b9fdfbaa661de36e0a930cae
MayurMansukh <Mansukhmayur99@gmail.com>
2021-03-04 11:32:39 +0530

    refactor display right output

--- src/main/java/MoodAnalyzerJunit/MoodAnalyser.java
+++ src/main/java/MoodAnalyzerJunit/MoodAnalyser.java
@@ -1,21 +1,15 @@
 package MoodAnalyzerJunit;
-
 public class MoodAnalyser {
     private String message;
 
-    public MoodAnalyser(String message)
-    {
-        this.message=message;
+    public MoodAnalyser(String message) {
+        this.message = message;
     }
 
-    public String analyseMood()
-    {
-        if(message.contains("sad"))
-        {
+    public String analyseMood() {
+        if (message.contains("sad")) {
             return "Sad";
-        }
-        else
-        {
+        } else {
             return "Happy";
         }
     }
